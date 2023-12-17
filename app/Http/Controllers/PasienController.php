ab37ed1f2accf58ba94ce31dceca761877225f07
Firman <firmancitranurrahman@gmail.com>
2023-12-17 21:09:42 +0700

    add function pelayanan

--- app/Http/Controllers/PasienController.php
+++ app/Http/Controllers/PasienController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pasien;
+use App\Models\Pelayanan;
+use App\Models\Pemeriksaan;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Http\Request;
 
 class PasienController extends Controller
@@ -13,7 +17,15 @@ class PasienController extends Controller
         $pasien= Pasien::all();
 
         return view('pasien.listdatapasien',['pasien'=>$pasien]);
+    }
 
+    public function listdatapelayanan(){
+        $query=DB::table('pemeriksaans as pem')
+        ->join('pasiens as pas', 'pem.pasien', '=', 'pas.id')
+        // ->join('orders', 'users.id', '=', 'orders.user_id')
+        ->select('pem.*', 'pas.name')->get();
+
+        return view('pasien.listdatapelayanan',['query'=>$query]);
     }
     public function tambahdatapasien(){
         return view('pasien.tambahpasien');
@@ -24,7 +36,6 @@ class PasienController extends Controller
             'name'=>'required',
             'umur'=>'required',
             'golongan_darah'=>'required',
-            'no_kk'=>'required',
             'tgl_lahir'=>'required',
             'pekerjaan'=>'required',
             'status_pernikahan'=>'required',
@@ -35,7 +46,7 @@ class PasienController extends Controller
             'name'=>$request->name,
             'umur'=>$request->umur,
             'golongan_darah'=>$request->golongan_darah,
-            'no_kk'=>$request->no_kk,
+            'nik'=>$request->nik,
             'tgl_lahir'=>$request->tgl_lahir,
             'pekerjaan'=>$request->pekerjaan,
             'status_pernikahan'=>$request->status_pernikahan,
@@ -43,29 +54,77 @@ class PasienController extends Controller
             'email'=>$request->email,
             
         ]);
-        return back();
+        return dd($pasien);
     }
     
     public function registerpelayanan(){
-      return view('pasien.registerpelayanan');
+        $pelayanan= Pelayanan::all();
+       
+        $currentDate = now();
+         // Membuat format nomor registrasi yang diinginkan
+    $noRegisterFormat = $currentDate->format('Ymd') . '%04d';
+
+    // Mendapatkan nomor registrasi terakhir yang digenerate
+    $lastRegister = Pemeriksaan::latest('no_registrasi')->first();
+
+    // Jika sudah ada nomor registrasi sebelumnya
+    if ($lastRegister) {
+        // Mendapatkan nomor registrasi terakhir yang digenerate
+        $lastRegisterNumber = substr($lastRegister->no_register, 8);
+
+        // Mengecek apakah tahun, bulan, dan hari sama dengan tanggal saat ini
+        if ($currentDate->isSameDay($lastRegister->created_at)) {
+            // Increment nomor registrasi
+            $nextRegisterNumber = intval($lastRegisterNumber) + 1;
+        } else {
+            // Jika tanggal berbeda, reset nomor registrasi
+            $nextRegisterNumber = 1;
+        }
+    } else {
+        // Jika belum ada nomor registrasi sebelumnya
+        $nextRegisterNumber = 1;
+    }
+
+    // Membuat nomor registrasi baru dengan format yang diinginkan
+    $noRegister = sprintf($noRegisterFormat, $nextRegisterNumber);
+
+
+        
+      return view('pasien.registerpelayanan',['pelayanan'=>$pelayanan,'noRegister'=>$noRegister]);
     }  
     
     public function ceknik(Request $request){
         $nik=$request->input('nik');
 
         $pasien = Pasien::where('nik', $nik)->first();
-        if ($pasien) {
+
+        if($pasien!== null){
             return response()->json([
-                'status' => 'available',
-                'nama' => $pasien->name,
-                'alamat' => $pasien->alamat,
-            ]);
-        } else {
-            return response()->json(['status' => 'unavailable']);
-        }
-
-
+                        'status' => true,
+                        'id' => $pasien->id,
+                        'nama' => $pasien->name,
+                        'alamat' => $pasien->alamat,
+                    ]);
+        }else{
+            return response()->json([
+                'status' =>false ]
+            );
+        }  
     }
+
+    public function prosespelayanan( Request $request){
+        $tgl_pemeriksaan= now();
+        $pemeriksaan = Pemeriksaan::create([
+            'no_registrasi'=>$request->no_registrasi,
+            'tgl_pemeriksaan'=>$tgl_pemeriksaan->toDateString(),
+            'spesialisasi'=>$request->spesialisasi,
+            'pasien'=>$request->pasien,
+            // 'status'=>$request->pemeriksaan
+        ]);
+
+        return dd($pemeriksaan);
+    }
+
 
 
 
